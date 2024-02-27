5dda7ac22e8f0fb8ced1744e9c433baf02d64662
pablo11-dev <pablofr2018@gmail.com>
2024-02-27 15:32:27 -0300

    pastaatt

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Itens from './screens/itens';
+export default function App() {
+  return (
+    <View>
+      <Itens/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ components/input.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TextInput, Text, View, StyleSheet } from 'react-native';
+
+const InputField = ({ label, value, setValue, placeholder }) => {
+  return (
+    <View>
+      <Text style={styles.text}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue}
+        value={value}
+        placeholder={placeholder}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: '95%',
+    margin: 12,
+    padding: 10,
+    borderWidth: 0,
+    borderColor: 'black',
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+
+  },
+  text: {
+    fontSize: 14,
+    color: 'black',
+    marginTop: 10,
+    marginLeft: 25,
+  },
+});
+
+export default InputField;
